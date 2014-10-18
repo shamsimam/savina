7aeeaca5ddeab9b9c255b8afb5d5c58d022ca7a5
Shams Imam <shams@rice.edu>
2014-10-18 17:03:29 -0500

    -- implementation of Successive Over-Relaxation benchmark

---
+++ src/main/java/edu/rice/habanero/benchmarks/sor/SucOverRelaxConfig.java
@@ -0,0 +1,99 @@
+package edu.rice.habanero.benchmarks.sor;
+
+import edu.rice.habanero.benchmarks.BenchmarkRunner;
+
+import java.util.Random;
+
+/**
+ * @author <a href="http://shams.web.rice.edu/">Shams Imam</a> (shams@rice.edu)
+ */
+public final class SucOverRelaxConfig {
+
+    // ORIGINAL val REF_VAL = Array[Double](0.498574406322512, 1.1234778980135105, 1.9954895063582696)
+    protected static final double[] REF_VAL = {
+            0.000003189420084871275,
+            0.001846644602759566,
+            0.0032099996270638005,
+            0.0050869220175413146,
+            0.008496328291240363,
+            0.016479973604143234,
+            0.026575660248076397,
+            1.026575660248076397,
+            2.026575660248076397,
+            3.026575660248076397};
+    protected static final int[] DATA_SIZES = {20, 80, 100, 120, 150, 200, 250, 300, 350, 400};
+    protected static final int JACOBI_NUM_ITER = 100;
+    protected static final double OMEGA = 1.25;
+    protected static final long RANDOM_SEED = 10101010;
+
+    protected static int N = 0;
+    protected static boolean debug = false;
+
+    protected static void parseArgs(final String[] args) {
+        int i = 0;
+        while (i < args.length) {
+            final String loopOptionKey = args[i];
+            switch (loopOptionKey) {
+                case "-n":
+                    i += 1;
+                    N = Integer.parseInt(args[i]);
+                    break;
+                case "-debug":
+                case "-verbose":
+                    debug = true;
+                    break;
+            }
+            i += 1;
+        }
+    }
+
+    protected static void printArgs() {
+        System.out.printf(BenchmarkRunner.argOutputFormat, "N (input size)", N);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Data size", DATA_SIZES[N]);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "debug", debug);
+    }
+
+    protected static void performComputation(final double theta) {
+        final double sint = Math.sin(theta);
+        final double res = sint * sint;
+        //defeat dead code elimination
+        if (res <= 0) {
+            throw new IllegalStateException("Benchmark exited with unrealistic res value " + res);
+        }
+    }
+
+    protected static Random R = null;
+    protected static double[][] A = null;
+
+    protected static void initialize() {
+        R = resetRandomGenerator();
+        final int dataSize = SucOverRelaxConfig.DATA_SIZES[SucOverRelaxConfig.N];
+        A = randomMatrix(dataSize, dataSize);
+    }
+
+    protected static Random resetRandomGenerator() {
+        return new Random(RANDOM_SEED);
+    }
+
+    protected static double[][] randomMatrix(final int M, final int N) {
+        final double[][] A = new double[M][N];
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = R.nextDouble() * 1e-6;
+            }
+        }
+
+        return A;
+    }
+
+    protected static void jgfValidate(final double gTotal, final int size) {
+        final double dev = Math.abs(gTotal - REF_VAL[size]);
+        if (dev > 1.0e-12) {
+            System.out.println("Validation failed");
+            System.out.println("Gtotal = " + gTotal + "  " + REF_VAL[size] + "  " + dev + "  " + size);
+        } else {
+            System.out.println("Validation OK!");
+        }
+    }
+}
---
+++ src/main/scala/edu/rice/habanero/benchmarks/sor/SucOverRelaxScalaActorBenchmark.scala
@@ -0,0 +1,301 @@
+package edu.rice.habanero.benchmarks.sor
+
+import edu.rice.habanero.actors.{ScalaActor, ScalaActorState}
+import edu.rice.habanero.benchmarks.{Benchmark, BenchmarkRunner}
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ *
+ * @author <a href="http://shams.web.rice.edu/">Shams Imam</a> (shams@rice.edu)
+ */
+object SucOverRelaxScalaActorBenchmark {
+
+  def main(args: Array[String]) {
+    BenchmarkRunner.runBenchmark(args, new SucOverRelaxScalaActorBenchmark)
+  }
+
+  private final class SucOverRelaxScalaActorBenchmark extends Benchmark {
+    def initialize(args: Array[String]) {
+      SucOverRelaxConfig.parseArgs(args)
+    }
+
+    def printArgInfo() {
+      SucOverRelaxConfig.printArgs()
+      SucOverRelaxConfig.initialize()
+    }
+
+    def runIteration() {
+      val dataLevel = SucOverRelaxConfig.N
+      val sorRunner = new SorRunner(dataLevel)
+      sorRunner.start()
+      sorRunner.send(SorBootMessage)
+
+      ScalaActorState.awaitTermination()
+    }
+
+    def cleanupIteration(lastIteration: Boolean, execTimeMillis: Double): Unit = {
+      SucOverRelaxConfig.initialize()
+    }
+  }
+
+  case class SorBorder(borderActors: Array[ScalaActor[AnyRef]])
+
+  case class SorBorderMessage(mBorder: SorBorder)
+
+  case class SorStartMessage(mi: Int, mActors: Array[ScalaActor[AnyRef]])
+
+  case class SorValueMessage(v: Double)
+
+  case object SorBootMessage
+
+  case class SorResultMessage(mx: Int, my: Int, mv: Double, msgRcv: Int)
+
+  private class SorRunner(n: Int) extends ScalaActor[AnyRef] {
+
+    private val s = SucOverRelaxConfig.DATA_SIZES(n)
+    private val part = s / 2
+    private val sorActors = Array.ofDim[ScalaActor[AnyRef]](s * (part + 1))
+
+    private def boot(): Unit = {
+
+      val myBorder = Array.ofDim[ScalaActor[AnyRef]](s)
+      val randoms = SucOverRelaxConfig.A
+
+      for (i <- 0 until s) {
+        var c = i % 2
+        for (j <- 0 until part) {
+          val pos = i * (part + 1) + j
+          c = 1 - c
+          sorActors(pos) = new SorActor(pos, randoms(i)(j), c, s, part + 1, SucOverRelaxConfig.OMEGA, this, false)
+          sorActors(pos).start()
+          if (j == (part - 1)) {
+            myBorder(i) = sorActors(pos)
+          }
+        }
+      }
+
+      val partialMatrix = Array.ofDim[Double](s, s - part)
+      for (i <- 0 until s) {
+        for (j <- 0 until s - part) {
+          partialMatrix(i)(j) = randoms(i)(j + part)
+        }
+      }
+
+      val sorPeer = new SorPeer(s, part, partialMatrix, SorBorder(myBorder), this)
+      sorPeer.start()
+      sorPeer.send(SorBootMessage)
+    }
+
+    private var gTotal = 0.0
+    private var returned = 0
+    private var totalMsgRcv = 0
+    private var expectingBoot = true
+
+    override def process(msg: AnyRef) {
+      msg match {
+        case SorBootMessage =>
+          expectingBoot = false
+          boot()
+        case SorResultMessage(mx, my, mv, msgRcv) =>
+          if (expectingBoot) {
+            throw new IllegalStateException("SorRunner not booted yet!")
+          }
+          totalMsgRcv += msgRcv
+          returned += 1
+          gTotal += mv
+          if (returned == (s * part) + 1) {
+            SucOverRelaxConfig.jgfValidate(gTotal, n)
+            exit()
+          }
+        case SorBorderMessage(mBorder) =>
+          if (expectingBoot) {
+            throw new IllegalStateException("SorRunner not booted yet!")
+          }
+          for (i <- 0 until s) {
+            sorActors((i + 1) * (part + 1) - 1) = mBorder.borderActors(i)
+          }
+          for (i <- 0 until s) {
+            for (j <- 0 until part) {
+              val pos = i * (part + 1) + j
+              sorActors(pos).send(SorStartMessage(SucOverRelaxConfig.JACOBI_NUM_ITER, sorActors))
+            }
+          }
+      }
+    }
+  }
+
+  class SorActor(
+                  pos: Int,
+                  var value: Double,
+                  color: Int,
+                  nx: Int,
+                  ny: Int,
+                  omega: Double,
+                  sorSource: ScalaActor[AnyRef],
+                  peer: Boolean
+                  ) extends ScalaActor[AnyRef] {
+
+    private val selfActor = this
+    private final val x = pos / ny
+    private final val y = pos % ny
+
+    private final val omega_over_four = 0.25 * omega
+    private final val one_minus_omega = 1.0 - omega
+
+    private final val neighbors: Array[Int] =
+      if (x > 0 && x < nx - 1 && y > 0 && y < ny - 1) {
+        val tempNeighbors = Array.ofDim[Int](4)
+        tempNeighbors(0) = calPos(x, y + 1)
+        tempNeighbors(1) = calPos(x + 1, y)
+        tempNeighbors(2) = calPos(x, y - 1)
+        tempNeighbors(3) = calPos(x - 1, y)
+        tempNeighbors
+      } else if ((x == 0 || x == (nx - 1)) && (y == 0 || y == (ny - 1))) {
+        val tempNeighbors = Array.ofDim[Int](2)
+        tempNeighbors(0) = if (x == 0) calPos(x + 1, y) else calPos(x - 1, y)
+        tempNeighbors(1) = if (y == 0) calPos(x, y + 1) else calPos(x, y - 1)
+        tempNeighbors
+      } else if ((x == 0 || x == (nx - 1)) || (y == 0 || y == (ny - 1))) {
+        val tempNeighbors = Array.ofDim[Int](3)
+        if (x == 0 || x == nx - 1) {
+          tempNeighbors(0) = if (x == 0) calPos(x + 1, y) else calPos(x - 1, y)
+          tempNeighbors(1) = calPos(x, y + 1)
+          tempNeighbors(2) = calPos(x, y - 1)
+        } else {
+          tempNeighbors(0) = if (y == 0) calPos(x, y + 1) else calPos(x, y - 1)
+          tempNeighbors(1) = calPos(x + 1, y)
+          tempNeighbors(2) = calPos(x - 1, y)
+        }
+        tempNeighbors
+      } else {
+        Array.ofDim[Int](0)
+      }
+
+    private def calPos(x1: Int, y1: Int): Int = {
+      x1 * ny + y1
+    }
+
+    private var iter = 0
+    private var maxIter = 0
+    private var msgRcv = 0
+    private var sorActors: Array[ScalaActor[AnyRef]] = null
+
+    private var receivedVals = 0
+    private var sum = 0.0
+    private var expectingStart = true
+    private val pendingMessages = new ListBuffer[AnyRef]()
+
+    override def process(msg: AnyRef) {
+      msg match {
+        case SorStartMessage(mi, mActors) =>
+          expectingStart = false
+          sorActors = mActors
+          maxIter = mi
+          if (color == 1) {
+            neighbors.foreach {
+              loopNeighIndex =>
+                sorActors(loopNeighIndex).send(SorValueMessage(value))
+            }
+            iter += 1
+            msgRcv += 1
+          }
+          pendingMessages.foreach {
+            loopMessage => selfActor.send(loopMessage)
+          }
+          pendingMessages.clear()
+
+        case message: SorValueMessage =>
+          if (expectingStart) {
+            pendingMessages.append(message)
+          } else {
+            msgRcv += 1
+            if (iter < maxIter) {
+              receivedVals += 1
+              sum += message.v
+              if (receivedVals == neighbors.length) {
+                value = (omega_over_four * sum) + (one_minus_omega * value)
+                sum = 0.0
+                receivedVals = 0
+
+                neighbors.foreach {
+                  loopNeighIndex =>
+                    sorActors(loopNeighIndex).send(SorValueMessage(value))
+                }
+                iter += 1
+              }
+              if (iter == maxIter) {
+                sorSource.send(SorResultMessage(x, y, value, msgRcv))
+                exit()
+              }
+            }
+          }
+      }
+    }
+  }
+
+  class SorPeer(
+                 s: Int,
+                 partStart: Int,
+                 matrixPart: Array[Array[Double]],
+                 border: SorBorder,
+                 sorSource: SorRunner
+                 ) extends ScalaActor[AnyRef] {
+
+    private val sorActors = Array.ofDim[ScalaActor[AnyRef]](s * (s - partStart + 1))
+
+    private def boot(): Unit = {
+      val myBorder = Array.ofDim[ScalaActor[AnyRef]](s)
+      for (i <- 0 until s) {
+        sorActors(i * (s - partStart + 1)) = border.borderActors(i)
+      }
+      for (i <- 0 until s) {
+        var c = (i + partStart) % 2
+        for (j <- 1 until (s - partStart + 1)) {
+          val pos = i * (s - partStart + 1) + j
+          c = 1 - c
+          sorActors(pos) = new SorActor(pos, matrixPart(i)(j - 1), c, s, s - partStart + 1,
+            SucOverRelaxConfig.OMEGA, this, true)
+          sorActors(pos).start()
+
+          if (j == 1) {
+            myBorder(i) = sorActors(pos)
+          }
+        }
+      }
+      sorSource.send(SorBorderMessage(SorBorder(myBorder)))
+
+      for (i <- 0 until s) {
+        for (j <- 1 until (s - partStart + 1)) {
+          val pos = i * (s - partStart + 1) + j
+          sorActors(pos).send(SorStartMessage(SucOverRelaxConfig.JACOBI_NUM_ITER, sorActors))
+        }
+      }
+    }
+
+    private var gTotal = 0.0
+    private var returned = 0
+    private var totalMsgRcv = 0
+    private var expectingBoot = true
+
+    override def process(msg: AnyRef) {
+      msg match {
+        case SorBootMessage =>
+          expectingBoot = false
+          boot()
+        case SorResultMessage(mx, my, mv, msgRcv) =>
+          if (expectingBoot) {
+            throw new IllegalStateException("SorPeer not booted yet!")
+          }
+          totalMsgRcv += msgRcv
+          returned += 1
+          gTotal += mv
+          if (returned == s * (s - partStart)) {
+            sorSource.send(SorResultMessage(-1, -1, gTotal, totalMsgRcv))
+            exit()
+          }
+      }
+    }
+  }
+
+}

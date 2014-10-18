e3967dd285cbc4f22f4fa7c8942f4cf6e59b4c16
Shams Imam <shams@rice.edu>
2014-10-18 17:01:07 -0500

    -- implementation of Online Facility Location benchmark

---
+++ src/main/java/edu/rice/habanero/benchmarks/facloc/FacilityLocationConfig.java
@@ -0,0 +1,224 @@
+package edu.rice.habanero.benchmarks.facloc;
+
+import edu.rice.habanero.benchmarks.BenchmarkRunner;
+
+import java.util.*;
+
+/**
+ * @author <a href="http://shams.web.rice.edu/">Shams Imam</a> (shams@rice.edu)
+ */
+public final class FacilityLocationConfig {
+
+    protected static int NUM_POINTS = 100_000;
+    protected static double GRID_SIZE = 500.0;
+    protected static double F = Math.sqrt(2) * GRID_SIZE;
+    protected static double ALPHA = 2.0;
+    protected static int CUTOFF_DEPTH = 3;
+    protected static long SEED = 123456L;
+
+    protected static int N = 40000;
+    protected static int C = 1;
+    protected static boolean debug = false;
+
+    protected static void parseArgs(final String[] args) {
+        int i = 0;
+        while (i < args.length) {
+            final String loopOptionKey = args[i];
+            if ("-n".equals(loopOptionKey)) {
+                i += 1;
+                NUM_POINTS = Integer.parseInt(args[i]);
+            } else if ("-g".equals(loopOptionKey)) {
+                i += 1;
+                GRID_SIZE = java.lang.Double.parseDouble(args[i]);
+            } else if ("-a".equals(loopOptionKey)) {
+                i += 1;
+                ALPHA = java.lang.Double.parseDouble(args[i]);
+            } else if ("-s".equals(loopOptionKey)) {
+                i += 1;
+                SEED = java.lang.Long.parseLong(args[i]);
+            } else if ("-c".equals(loopOptionKey)) {
+                i += 1;
+                CUTOFF_DEPTH = Integer.parseInt(args[i]);
+            } else if ("-debug".equals(loopOptionKey) || "-verbose".equals(loopOptionKey)) {
+                debug = true;
+            }
+            i += 1;
+        }
+        F = Math.sqrt(2) * GRID_SIZE;
+    }
+
+    protected static void printArgs() {
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Num points", NUM_POINTS);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Grid size", GRID_SIZE);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "F", F);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Alpha", ALPHA);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Cut-off depth", CUTOFF_DEPTH);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "Seed", SEED);
+        System.out.printf(BenchmarkRunner.argOutputFormat, "debug", debug);
+    }
+
+    protected static class Point {
+
+        private static Random r = new Random();
+
+        protected static void setSeed(final long seed) {
+            r = new Random(seed);
+        }
+
+        protected static Point random(final double gridSize) {
+            final double x = r.nextDouble() * gridSize;
+            final double y = r.nextDouble() * gridSize;
+            return new Point(x, y);
+        }
+
+        protected static Point findCenter(final Collection<Point> points) {
+            double sumX = 0, sumY = 0;
+
+            for (final Point point : points) {
+                sumX += point.x;
+                sumY += point.y;
+            }
+
+            final int numPoints = points.size();
+            final double avgX = sumX / numPoints;
+            final double avgY = sumY / numPoints;
+            return new Point(avgX, avgY);
+        }
+
+        protected final double x;
+        protected final double y;
+
+        public Point(final double x, final double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getDistance(final Point p) {
+            final double xDiff = p.x - x;
+            final double yDiff = p.y - y;
+            final double distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+            return distance;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%5.2f, %5.2f)", x, y);
+        }
+    }
+
+    protected static class Facility {
+
+        public static List<Facility> split(final Facility f) {
+            // split points into four categories
+            @SuppressWarnings("unchecked")
+            final Set<Point>[] pointSplit = new Set[]{
+                    new HashSet<Point>(),
+                    new HashSet<Point>(),
+                    new HashSet<Point>(),
+                    new HashSet<Point>()
+            };
+            final Point tempCenter = Point.findCenter(f.points);
+            for (final Point loopPoint : f.points) {
+                final double xDiff = loopPoint.x - tempCenter.x;
+                final double yDiff = loopPoint.y - tempCenter.y;
+                if (xDiff > 0 && yDiff > 0) {
+                    pointSplit[0].add(loopPoint);
+                } else if (xDiff < 0 && yDiff > 0) {
+                    pointSplit[1].add(loopPoint);
+                } else if (xDiff < 0 && yDiff < 0) {
+                    pointSplit[2].add(loopPoint);
+                } else {
+                    pointSplit[3].add(loopPoint);
+                }
+            }
+
+            if (debug) {
+                System.out.println(
+                        "split: " + f.points.size() + " into " + pointSplit[0].size() + ", " + pointSplit[1].size() +
+                                ", " + pointSplit[2].size() + " and " + pointSplit[3].size());
+            }
+
+            // create new facilities
+            final Facility f1 = makeFacility(pointSplit[0]);
+            final Facility f2 = makeFacility(pointSplit[1]);
+            final Facility f3 = makeFacility(pointSplit[2]);
+            final Facility f4 = makeFacility(pointSplit[3]);
+
+            return Arrays.asList(f1, f2, f3, f4);
+        }
+
+        private static Facility makeFacility(final Collection<Point> points) {
+            final Point newCenter = Point.findCenter(points);
+            final Facility newFacility = new Facility(newCenter);
+
+            for (final Point loopPoint : points) {
+                newFacility.addPoint(loopPoint);
+            }
+
+            return newFacility;
+        }
+
+        public final Point center;
+        private double distance = 0.0;
+        private double maxDistance = 0.0;
+        private Set<Point> points = new HashSet<Point>();
+
+        protected Facility(final Point center) {
+            this.center = center;
+        }
+
+        public void addPoint(final Point p) {
+            final double d = center.getDistance(p);
+            if (d > maxDistance) {
+                maxDistance = d;
+            }
+            distance += d;
+            points.add(p);
+        }
+
+        public int numPoints() {
+            return points.size();
+        }
+
+        public double getTotalDistance() {
+            return distance;
+        }
+
+        @Override
+        public String toString() {
+            return "Facility{center: " + center + ", distance: " + distance + ", num-pts: " + points.size() + "}";
+        }
+    }
+
+    protected static class Box {
+
+        final double x1;
+        final double y1;
+        final double x2;
+        final double y2;
+
+        protected Box(final double x1, final double y1, final double x2, final double y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public boolean contains(final Point p) {
+            return (x1 <= p.x && y1 <= p.y && p.x <= x2 && p.y <= y2);
+        }
+
+        public Point midPoint() {
+            return new Point((x1 + x2) / 2, (y1 + y2) / 2);
+        }
+    }
+
+    protected static interface Position {
+        int UNKNOWN = -2;
+        int ROOT = -1;
+        int TOP_LEFT = 0;
+        int TOP_RIGHT = 1;
+        int BOT_LEFT = 2;
+        int BOT_RIGHT = 3;
+    }
+}
---
+++ src/main/scala/edu/rice/habanero/benchmarks/facloc/FacilityLocationHabaneroActorBenchmark.scala
@@ -0,0 +1,329 @@
+package edu.rice.habanero.benchmarks.facloc
+
+import java.util.function.Consumer
+
+import edu.rice.habanero.actors.HabaneroActor
+import edu.rice.habanero.benchmarks.facloc.FacilityLocationConfig.{Box, Point, Position}
+import edu.rice.habanero.benchmarks.{Benchmark, BenchmarkRunner}
+import edu.rice.hj.Module0._
+import edu.rice.hj.api.HjSuspendable
+
+/**
+ *
+ * @author <a href="http://shams.web.rice.edu/">Shams Imam</a> (shams@rice.edu)
+ */
+object FacilityLocationHabaneroActorBenchmark {
+
+  def main(args: Array[String]) {
+    BenchmarkRunner.runBenchmark(args, new FacilityLocationHabaneroActorBenchmark)
+  }
+
+  private final class FacilityLocationHabaneroActorBenchmark extends Benchmark {
+    def initialize(args: Array[String]) {
+      FacilityLocationConfig.parseArgs(args)
+    }
+
+    def printArgInfo() {
+      FacilityLocationConfig.printArgs()
+    }
+
+    def runIteration() {
+      finish(new HjSuspendable {
+        override def run() = {
+
+          val threshold = FacilityLocationConfig.ALPHA * FacilityLocationConfig.F
+          val boundingBox = new Box(0, 0, FacilityLocationConfig.GRID_SIZE, FacilityLocationConfig.GRID_SIZE)
+
+          val rootQuadrant = new QuadrantActor(
+            null, Position.ROOT, boundingBox, threshold, 0,
+            new java.util.ArrayList[Point](), 1, -1, new java.util.ArrayList[Point]())
+          rootQuadrant.start()
+
+          val producer = new ProducerActor(rootQuadrant)
+          producer.start()
+
+        }
+      })
+    }
+
+    def cleanupIteration(lastIteration: Boolean, execTimeMillis: Double) {
+    }
+  }
+
+
+  private abstract class Msg()
+
+  private case class FacilityMsg(positionRelativeToParent: Int, depth: Int, point: Point, fromChild: Boolean) extends Msg
+
+  private case class NextCustomerMsg() extends Msg
+
+  private case class CustomerMsg(producer: HabaneroActor[AnyRef], point: Point) extends Msg
+
+  private case class RequestExitMsg() extends Msg
+
+  private case class ConfirmExitMsg(facilities: Int, supportCustomers: Int) extends Msg
+
+
+  private class ProducerActor(consumer: HabaneroActor[AnyRef]) extends HabaneroActor[AnyRef] {
+
+    private val selfActor = this
+    private var itemsProduced = 0
+
+    override def onPostStart(): Unit = {
+      produceCustomer()
+    }
+
+    private def produceCustomer(): Unit = {
+      consumer.send(CustomerMsg(selfActor, Point.random(FacilityLocationConfig.GRID_SIZE)))
+      itemsProduced += 1
+    }
+
+    override def process(message: AnyRef) {
+      message match {
+        case msg: NextCustomerMsg =>
+          if (itemsProduced < FacilityLocationConfig.NUM_POINTS) {
+            produceCustomer()
+          } else {
+            consumer.send(RequestExitMsg())
+            exit()
+          }
+      }
+    }
+  }
+
+  private class QuadrantActor(parent: QuadrantActor,
+                              positionRelativeToParent: Int,
+                              val boundary: Box,
+                              threshold: Double,
+                              depth: Int,
+                              initLocalFacilities: java.util.List[Point],
+                              initKnownFacilities: Int,
+                              initMaxDepthOfKnownOpenFacility: Int,
+                              initCustomers: java.util.List[Point]) extends HabaneroActor[AnyRef] {
+
+    private val selfActor = this
+    // the facility associated with this quadrant if it were to open
+    private val facility: Point = boundary.midPoint()
+
+    // all the local facilities from corner ancestors
+    val localFacilities = new java.util.ArrayList[Point]()
+    localFacilities.addAll(initLocalFacilities)
+    localFacilities.add(facility)
+
+    private var knownFacilities = initKnownFacilities
+    private var maxDepthOfKnownOpenFacility = initMaxDepthOfKnownOpenFacility
+    private var terminatedChildCount = 0
+
+    // the support customers for this Quadrant
+    private val supportCustomers = new java.util.ArrayList[Point]()
+
+    private var childrenFacilities = 0
+    private var facilityCustomers = 0
+
+    // null when closed, non-null when open
+    private var children: List[QuadrantActor] = null
+    private var childrenBoundaries: List[Box] = null
+
+    // the cost so far
+    private var totalCost = 0.0
+
+    initCustomers.forEach(new Consumer[Point] {
+      override def accept(loopPoint: Point): Unit = {
+        if (boundary.contains(loopPoint)) {
+          addCustomer(loopPoint)
+        }
+      }
+
+      override def andThen(after: Consumer[_ >: Point]): Consumer[Point] = {
+        this
+      }
+    })
+
+    override def process(msg: AnyRef) {
+      msg match {
+        case customer: CustomerMsg =>
+
+          val point: Point = customer.point
+          if (children == null) {
+
+            // no open facility
+            addCustomer(point)
+            if (totalCost > threshold) {
+              partition()
+            }
+
+          } else {
+
+            // a facility is already open, propagate customer to correct child
+            var index = 0
+            while (index <= 4) {
+              val loopChildBoundary = childrenBoundaries(index)
+              if (loopChildBoundary.contains(point)) {
+                children(index).send(customer)
+                index = 5
+              } else {
+                index += 1
+              }
+            }
+          }
+
+          if (parent eq null) {
+            // request next customer
+            customer.producer.send(NextCustomerMsg())
+          }
+
+        case facility: FacilityMsg =>
+
+          val point = facility.point
+          val fromChild = facility.fromChild
+
+          knownFacilities += 1
+          localFacilities.add(point)
+
+          if (fromChild) {
+            notifyParentOfFacility(point, facility.depth)
+            if (facility.depth > maxDepthOfKnownOpenFacility) {
+              maxDepthOfKnownOpenFacility = facility.depth
+            }
+
+            // notify sibling
+            val childPos = facility.positionRelativeToParent
+            val siblingPos: Int = if (childPos == Position.TOP_LEFT) {
+              Position.BOT_RIGHT
+            } else if (childPos == Position.TOP_RIGHT) {
+              Position.BOT_LEFT
+            } else if (childPos == Position.BOT_RIGHT) {
+              Position.TOP_LEFT
+            } else {
+              Position.TOP_RIGHT
+            }
+            children(siblingPos).send(FacilityMsg(Position.UNKNOWN, depth, point, false))
+
+          } else {
+
+            // notify all children
+            if (children ne null) {
+              children.foreach {
+                loopChild =>
+                  loopChild.send(FacilityMsg(Position.UNKNOWN, depth, point, false))
+              }
+            }
+          }
+
+        case exitMsg: RequestExitMsg =>
+
+          if (children ne null) {
+            children.foreach {
+              loopChild =>
+                loopChild.send(exitMsg)
+            }
+          } else {
+            // No children, notify parent and safely exit
+            safelyExit()
+          }
+
+        case exitMsg: ConfirmExitMsg =>
+
+          // child has sent a confirmation that it has exited
+          terminatedChildCount += 1
+
+          childrenFacilities += exitMsg.facilities
+          facilityCustomers += exitMsg.supportCustomers
+
+          if (terminatedChildCount == 4) {
+            // all children terminated
+            safelyExit()
+          }
+      }
+    }
+
+    private def addCustomer(point: Point): Unit = {
+      supportCustomers.add(point)
+      val minCost = findCost(point)
+      totalCost += minCost
+    }
+
+    private def findCost(point: Point): Double = {
+      var result = Double.MaxValue
+
+      // there will be at least one facility
+      localFacilities.forEach(new Consumer[Point] {
+        override def accept(loopPoint: Point): Unit = {
+          val distance = loopPoint.getDistance(point)
+          if (distance < result) {
+            result = distance
+          }
+        }
+
+        override def andThen(after: Consumer[_ >: Point]): Consumer[Point] = {
+          this
+        }
+      })
+
+      result
+    }
+
+    private def notifyParentOfFacility(p: Point, depth: Int): Unit = {
+      //println("Quadrant-" + id + ": notifyParentOfFacility: parent = " + parent)
+      if (parent ne null) {
+        //println("Quadrant-" + id + ": notifyParentOfFacility: sending msg to parent: " + parent.id)
+        parent.send(FacilityMsg(positionRelativeToParent, depth, p, true))
+      }
+    }
+
+    private def partition(): Unit = {
+
+      // notify parent that opened a new facility
+      notifyParentOfFacility(facility, depth)
+      maxDepthOfKnownOpenFacility = math.max(maxDepthOfKnownOpenFacility, depth)
+
+      // create children and propagate their share of customers to them
+      val firstBoundary: Box = new Box(boundary.x1, facility.y, facility.x, boundary.y2)
+      val secondBoundary: Box = new Box(facility.x, facility.y, boundary.x2, boundary.y2)
+      val thirdBoundary: Box = new Box(boundary.x1, boundary.y1, facility.x, facility.y)
+      val fourthBoundary: Box = new Box(facility.x, boundary.y1, boundary.x2, facility.y)
+
+      val firstChild = new QuadrantActor(
+        selfActor, Position.TOP_LEFT, firstBoundary, threshold, depth + 1,
+        localFacilities, knownFacilities, maxDepthOfKnownOpenFacility, supportCustomers)
+      firstChild.start()
+
+      val secondChild = new QuadrantActor(
+        selfActor, Position.TOP_RIGHT, secondBoundary, threshold, depth + 1,
+        localFacilities, knownFacilities, maxDepthOfKnownOpenFacility, supportCustomers)
+      secondChild.start()
+
+      val thirdChild = new QuadrantActor(
+        selfActor, Position.BOT_LEFT, thirdBoundary, threshold, depth + 1,
+        localFacilities, knownFacilities, maxDepthOfKnownOpenFacility, supportCustomers)
+      thirdChild.start()
+
+      val fourthChild = new QuadrantActor(
+        selfActor, Position.BOT_RIGHT, fourthBoundary, threshold, depth + 1,
+        localFacilities, knownFacilities, maxDepthOfKnownOpenFacility, supportCustomers)
+      fourthChild.start()
+
+
+      children = List[QuadrantActor](firstChild, secondChild, thirdChild, fourthChild)
+      childrenBoundaries = List[Box](firstBoundary, secondBoundary, thirdBoundary, fourthBoundary)
+
+      // support customers have been distributed to the children
+      supportCustomers.clear()
+    }
+
+    private def safelyExit(): Unit = {
+
+      if (parent ne null) {
+        val numFacilities = if (children ne null) childrenFacilities + 1 else childrenFacilities
+        val numCustomers = facilityCustomers + supportCustomers.size
+        parent.send(ConfirmExitMsg(numFacilities, numCustomers))
+      } else {
+        val numFacilities = childrenFacilities + 1
+        println("  Num Facilities: " + numFacilities + ", Num customers: " + facilityCustomers)
+      }
+      exit()
+
+    }
+  }
+
+}
